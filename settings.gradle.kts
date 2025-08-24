519922765b0d37176d50a7840837f3aaa033ff74
zvio-coder <zvio@hotmail.co.uk>
2025-08-24 11:14:32 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        // Pin plugin versions so CI can resolve them
+        // Pin plugin versions so CI resolves them reliably
         id("com.android.application") version "8.5.2"
         id("org.jetbrains.kotlin.android") version "1.9.24"
     }
@@ -14,9 +14,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // Only the repos we actually need
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") } // ✅ Needed for FFmpegKit
     }
 }
 
